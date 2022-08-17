d867ef840477ce49f435ac07cccebc61ea18e882
ThomasUE4 <83637460+ThomasUE4@users.noreply.github.com>
2022-08-17 11:13:48 +0530

    added

---
+++ components/SearchBar.js
@@ -0,0 +1,202 @@
+
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import SearchRightButtons from './SearchRightButtons';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: '40rem',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  
+  
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '50ch',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+ 
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      
+    >
+      <MenuItem>Profile</MenuItem>
+      <MenuItem>My account</MenuItem>
+    </Menu>
+  );
+
+  
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+     
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+       <div className="flex justify-center"> 
+        <Toolbar>
+          
+          <Typography
+            variant="h6"
+            className="pr-3"
+            component="div"
+            sx={{ display: { xs:'block' } }}
+          >
+            Blipkart
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <div>
+              <StyledInputBase
+                placeholder="Search for products, brands and more"
+                inputProps={{ 'aria-label': 'search' }} 
+            
+              />
+            </div>
+          </Search>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+           
+            <SearchRightButtons/>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+            
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        
+        </Toolbar>
+       </div> 
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
+
+   
---
+++ components/Sections.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Image from 'next/image';
+
+const Sections = () => {
+  return (
+    <div className="flex justify-evenly mx-[4rem]">
+      
+       <div className="flex flex-col items-center font-semibold py-4">
+         <div className="w-[62px] h-[62px]">
+          <Image 
+           src="https://images.unsplash.com/photo-1602566178436-8cf72756f4cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+           width={900}
+           height={900}   
+           className="object-cover"  
+          />
+         </div>
+         <div> 
+          <h3>Top Offers</h3>
+         </div> 
+       </div>
+       <div className="py-4 flex flex-col items-center font-semibold">        
+         <div className="w-[62px] h-[62px]">
+           <Image 
+            src="https://images.unsplash.com/photo-1588964895597-cfccd6e2dbf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+            width={900}
+            height={900}
+            className="object-cover"
+           />
+         </div>
+         <div>
+           <h3>Grocery</h3>
+         </div>
+       </div>
+       <div className="py-4 flex flex-col items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+          <Image 
+            src="https://images.unsplash.com/photo-1589894404892-7310b92ea7a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+            width={900}
+            height={900}
+            className="object-cover"
+          />
+         </div>
+         <div> 
+          <h3>Mobiles</h3>
+         </div> 
+       </div>
+       <div className="py-4 flex flex-col items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+            <Image 
+               src="https://images.unsplash.com/photo-1638656749922-9158fd414393?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+               width={900}
+               height={900}
+               className="object-cover"
+            />
+         </div>  
+         <div className="flex">
+            <h3>Fashion</h3>
+            <ExpandMoreIcon sx={{fontSize: 15}} className="mt-[0.5rem] ml-[0.2rem]"/>
+         </div>
+       </div>
+       <div className="flex flex-col py-4 items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+            <Image 
+            src="https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1201&q=80"
+            width={900}
+            height={900}
+            className="object-cover"
+            />
+         </div>
+         <div className="flex">
+            <h3>Electronics</h3> 
+            <ExpandMoreIcon sx={{fontSize: 15}} className="mt-[0.5rem] ml-[0.2rem]" />
+         </div>      
+       </div>
+       <div className="flex flex-col py-4 items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+            <Image 
+               src="https://images.unsplash.com/photo-1600121848594-d8644e57abab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+               width={900}
+               height={900}
+               className="object-cover"
+            />
+         </div>  
+         <div className="flex">
+            <h3>Home</h3> 
+            <ExpandMoreIcon sx={{fontSize: 15}} className="mt-[0.5rem] ml-[0.2rem]"/>
+         </div>  
+       </div>
+       <div className="flex flex-col py-4 items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+          <Image
+             src="https://images.unsplash.com/photo-1584143987552-0ab1f595af6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+             width={900}
+             height={900}
+             className="object-cover"
+          /> 
+         </div>
+         <div>   
+          <h3>Appliances</h3>
+         </div> 
+       </div>
+       <div className="flex flex-col py-4 items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+          <Image
+             src="https://images.unsplash.com/photo-1530521954074-e64f6810b32d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+             width={900}
+             height={900}
+             className="object-cover"
+          />   
+         </div>
+         <div> 
+          <h3>Travel</h3>
+         </div>  
+       </div>
+       <div className="flex flex-col py-4 items-center font-semibold">
+         <div className="h-[62px] w-[62px]">
+            <Image
+               src="https://images.unsplash.com/photo-1559454403-b8fb88521f11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+               width={900}
+               height={900}
+               className="object-cover"
+            /> 
+         </div>
+         <div className="flex">  
+            <h3>Beauty, Toys & More</h3> 
+            <ExpandMoreIcon sx={{fontSize: 15}} className="mt-[0.5rem] ml-[0.2rem]"/>
+         </div>    
+       </div>
+    </div>
+  );
+}
+
+export default Sections
